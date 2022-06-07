0b65270e74f34880889c9664201d50c2669e6399
EmmaPrats <jazzemmapratsicamps@gmail.com>
2022-06-07 21:26:43 +0200

    Can load and play sounds.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,23 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+const char* WINDOW_TITLE = "La Cueva de los Condenados";
+
 SDL_Window* window = NULL;
 SDL_Surface* screenSurface = NULL;
 
 #define FPS 60
 int lastTime = 0, currentTime, deltaTime;
 float msFrame = 1 / (FPS / 1000.0f);
+
+const char* deathSoundFile = "resources/death.wav";
+const char* gameOverSoundFile = "resources/gameover.wav";
+const char* monsterSnoringSoundFile = "resources/Monster-Snoring.wav";
+const char* hitWallSoundFile = "resources/shock-wall.wav";
+const char* stepHumanSoundFile = "resources/step-human.wav";
+const char* stepMonsterSoundFile = "resources/step-monster.wav";
+const char* victorySoundFile = "resources/victory.wav";
+const char* waterfallSoundFile = "resources/waterfall.wav";
 
 bool initSDL();
 void init3D();
@@ -77,7 +88,7 @@ bool initSDL()
 	}
 	
 	window = SDL_CreateWindow(
-		"Dancing Torus",
+		WINDOW_TITLE,
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
 		SCREEN_WIDTH,
@@ -104,12 +115,22 @@ void initMusic()
 	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024); //or 4096?
 	Mix_Init(MIX_INIT_OGG);
 
-	//TODO load audio clips.
+	Mix_Chunk* waterfall = Mix_LoadWAV(waterfallSoundFile);
+	Mix_Chunk* deathSound = Mix_LoadWAV(deathSoundFile);
+	Mix_Chunk* gameOverSound = Mix_LoadWAV(gameOverSoundFile);
+	Mix_Chunk* monsterSnoringSound = Mix_LoadWAV(monsterSnoringSoundFile);
+	Mix_Chunk* hitWallSound = Mix_LoadWAV(hitWallSoundFile);
+	Mix_Chunk* stepHumanSound = Mix_LoadWAV(stepHumanSoundFile);
+	Mix_Chunk* stepMonsterSound = Mix_LoadWAV(stepMonsterSoundFile);
+	Mix_Chunk* victorySound = Mix_LoadWAV(victorySoundFile);
+	Mix_Chunk* waterfallSound = Mix_LoadWAV(waterfallSoundFile);
 
-	//mySong = Mix_LoadMUS("resources/Blastculture-Gravitation.ogg");
-	//if (!mySong)
-	//{
-	//	std::cout << "Error loading Music: " << Mix_GetError() << std::endl;
+	if (!waterfall)
+		printf("Error loading waterfall.");
+
+	int waterfallChannel = Mix_PlayChannel(-1, waterfall, -1);
+	Mix_SetPosition(waterfallChannel, 180, 10);
+
 	//	close();
 	//	exit(1);
 	//}
